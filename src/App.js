01b4e04af79763f6044644ed38318f8980ede4d0
HD111995 <shomawmisho1234@gmail.com>
2021-09-21 16:58:06 +0200

    fourth

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 
 import './App.css';
 import News from './components/News';
-import {
-  BrowserRouter as Router,
-  
-} from "react-router-dom";
 
 
 function App() {
   return (
-    <Router>
+   
       <News/>
-      </Router>
+      
   );
 }
 
